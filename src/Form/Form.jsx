3d8d1b079de73f530496c2c9c07fdc4bf0a8ea48
Aytac Karkin <152106942+Aytac-Karkin@users.noreply.github.com>
2024-01-12 12:19:44 +0100

    initial commit

---
+++ src/Form/Form.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+function Form(onAddActivity) {
+  const [activity, setActivity] = useState({
+    name: "",
+    isForGoodWeather: "false",
+  });
+  function handleChange(event) {
+    setActivity({ ...activity, name: event.target.value }); //greift auf den Wert im inputfeld zu
+  }
+
+  function handleCheck(event) {
+    setActivity({ ...activity, isForGoodWeather: event.target.checked });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // onAddActivity(activity);
+    setActivity({ name: "", isForGoodWeather: "false" });
+  }
+
+  console.log(activity);
+
+  return (
+    <>
+      <h1>Add new Activity</h1>
+      <form>
+        <div>
+          <label htmlFor="activityName">Name: </label>
+          <input
+            type="text"
+            id="activityName"
+            name="name"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="goodWeatherActivity">Good-weather activity: </label>
+          <input
+            type="checkbox"
+            id="goodWeatherActivity"
+            name="isForGoodWeather"
+            onChange={handleCheck}
+          />
+        </div>
+        <div>
+          <button type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default Form;
